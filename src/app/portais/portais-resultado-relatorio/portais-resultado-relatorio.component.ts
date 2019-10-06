1e967f21407f93be07db17f0255b0a9772465ee5
heitormach <heitormach@gmail.com>
2019-10-06 18:03:58 -0300

    [HEITOR] - Correções Relatório

--- src/app/portais/portais-resultado-relatorio/portais-resultado-relatorio.component.ts
+++ src/app/portais/portais-resultado-relatorio/portais-resultado-relatorio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RelatorioModel } from 'src/app/core/models/relatorio.model';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-portais-resultado-relatorio',
@@ -7,11 +8,17 @@ import { RelatorioModel } from 'src/app/core/models/relatorio.model';
   styleUrls: ['./portais-resultado-relatorio.component.scss']
 })
 export class PortaisResultadoRelatorioComponent implements OnInit {
-  constructor() { }
+  constructor(
+    private sanitizer: DomSanitizer
+  ) { }
 
   @Input() relatorio: RelatorioModel;
 
   ngOnInit() {
+  }
+
+  exibirFoto() {
+    return this.sanitizer.bypassSecurityTrustUrl('data:Image/*;base64,' + this.relatorio.detran.dadosDaImgCnh.foto);
   }
 
 }
